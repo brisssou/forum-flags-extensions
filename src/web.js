b96ae4881ea310e44218e1e987f1529d3dc2a4a1
brisssou <brice.laurencin@gmail.com>
2010-11-30 16:42:54 +0000

    * ajout des styles * ajout d'option pour l'animations de l'icone sur nouveau topic non lu * passage de la gestion des options par évenements

--- src/web.js
+++ src/web.js
@@ -24,17 +24,21 @@ function getPatern() {
   }
 }
 
-function goToHfr(){
+function goToPage(url) {
   chrome.tabs.getAllInWindow(undefined, function(tabs){
 
   for (var i = 0, tab; tab = tabs[i]; i++) {
-      if (tab.url && tab.url == getUsedURL()) {
+      if (tab.url && tab.url == url) {
         chrome.tabs.update(tab.id, {selected: true});
         return;
       }
     }
-    chrome.tabs.create({url: getUsedURL()});
+    chrome.tabs.create({url: url});
   });
+}
+
+function goToHfr(){
+  goToPage(getUsedURL());
 }
 
 function updateBadge(nbUnread) {
