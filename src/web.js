c299a48450c1a0dbc1d1c563a7a1fd3244abab89
brisssou <brice.laurencin@gmail.com>
2011-01-23 16:50:07 +0000

    * Intégration de la font Ubuntu tout partout, parce que je l'aime bien, et que personne aimait l'ancienne font (celle par défaut du navigateur) * Dans la popUp, il y a un tooltip sur les liens vers les topics. Il indique le nombre de nouvelles pages depuis le dernier passage sur le topic. * Utilisation du drapeau noir de lezioul * Récupération des catégories avec une RegEx

--- src/web.js
+++ src/web.js
@@ -3,8 +3,11 @@ var HFR_MY_DRAPS = HFR + "/forum1f.php?config=hfr.inc&owntopic=1&new=0&nojs=0";
 var HFR_MY_FAVS = HFR + "/forum1f.php?config=hfr.inc&owntopic=3&new=0&nojs=0";
 var HFR_MP = HFR + "/forum1f.php?config=hfr.inc&cat=prive";
 
-var UNREAD_REX = /title="Sujet n°\d+">([^<]+).+sujetCase5"><a href="([^"]+)/g;
+var UNREAD_REX = /title="Sujet n°\d+">([^<]+).+>(\d+)<\/a>.+sujetCase5"><a href="([^"]+).+Aller au dernier message lu sur ce sujet \(p.(\d+)\)/g;
 var MP_REX = /class="red">Vous avez (\d) nouveau/;
+//<select name="cat" onchange="document.getElementById('goto').submit()"><option value="1" >Hardware</option><option value="16" >Hardware - Périphériques</option><option value="15" >Ordinateurs portables</option><option value="23" >Technologies Mobiles</option><option value="2" >Overclocking, Cooling &amp; Tuning</option><option value="25" >Apple</option><option value="3" >Video &amp; Son</option><option value="14" >Photo numérique</option><option value="5" >Jeux Video</option><option value="4" >Windows &amp; Software</option><option value="22" >Réseaux grand public / SoHo</option><option value="21" >Systèmes &amp; Réseaux Pro</option><option value="11" >OS Alternatifs</option><option value="10" >Programmation</option><option value="12" >Graphisme</option><option value="6" >Achats &amp; Ventes</option><option value="8" >Emploi &amp; Etudes</option><option value="9" >Seti et projets distribués</option><option value="13" >Discussions</option><option value="prive" >Messages privés</option></select><input type="hidden" name="config" value="hfr.inc" /><input type="submit" value="Go" /></div></form> 
+var CATS_MASTER_REX = /<select name="cat"(.+)<\/select>/;
+var CATS_REX = /<option value="([^"]+)" >([^"]+)<\/option>/g;
 
 var ENTRY_URL_REX = /cat=(\d+)&amp;(subcat=(\d+)&amp;)?post=(\d+)&amp;page=(\d+)/;
 
@@ -58,17 +61,17 @@ function getUnreadCount(onSuccess, onError) {
         var popupContent = chrome.extension.getBackgroundPage().popupContent;
         popupContent.clear();
         var matches = null;
-        var muted = getPref(MUTED_TOPICS).split('|');;
+        var muted = getPref(MUTED_TOPICS).split('|');
         while (matches = UNREAD_REX.exec(content)) {
           debug("found one");
-          var url = matches[2];
+          var url = matches[3];
           var urlMatch = ENTRY_URL_REX.exec(url);
           if (urlMatch != null && !isMuted(urlMatch[1], urlMatch[4])) {
             unreadCount++;
-            popupContent.add(matches[1], urlMatch[1], urlMatch[4], url);
+            popupContent.add(matches[1], urlMatch[1], urlMatch[4], url, parseInt(matches[2]) - parseInt(matches[4]));
           } else {
             debug("... but a muted one");
-	  }
+          }
         }
         var mps = MP_REX.exec(content);
         if (mps != null) {
@@ -76,6 +79,17 @@ function getUnreadCount(onSuccess, onError) {
           debug("found "+mpsNb+" private messages");
           unreadCount += mpsNb;
           popupContent.setMps(mpsNb);
+        }
+        if (bg.cats == null || bg.cats.length == 0) {
+          bg.cats = new Array();
+          matches = CATS_MASTER_REX.exec(content);
+          if (matches!=null) {
+            var catsString = matches[0];
+            while (matches = CATS_REX.exec(catsString)) {
+              bg.cats[matches[1]] = matches[2];
+              debug("new cat :"+matches[1]+"/"+matches[2]);
+            }
+          }
         }
         handleSuccess(unreadCount);
         return;
