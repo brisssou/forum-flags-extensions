8eee498f15fba7a0fb5c35c86ec1f4fe2288d112
brisssou <brice.laurencin@gmail.com>
2011-06-23 19:43:36 +0000

    * limit on refresh time * limit on max opening tabs

--- hfr-drap4chrome/prefs.js
+++ hfr-drap4chrome/prefs.js
@@ -9,6 +9,7 @@ var DEBUG_ON = "debug_on";
 var OPEN_CAT = "open_cat";
 var MUTED_TOPICS = "muted_topics";
 var BG_COLOR = "bg_color";
+var MAX_OPEN_ALL = "max_open_all";
 
 var PREFS_DEFAULT = new Object();
 PREFS_DEFAULT[USE_DIRECT_LINK] = 'false';
@@ -21,6 +22,7 @@ PREFS_DEFAULT[DEBUG_ON] = 'false';
 PREFS_DEFAULT[OPEN_CAT] = 'true';
 PREFS_DEFAULT[MUTED_TOPICS] = '';
 PREFS_DEFAULT[BG_COLOR] = '#2F3740';
+PREFS_DEFAULT[MAX_OPEN_ALL] = 10;
 
 function getPref(name) {
   var value = localStorage[name];
