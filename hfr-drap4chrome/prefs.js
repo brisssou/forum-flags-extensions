6a081b0392fbae92193f1131802210e3a44b9804
brisssou <brice.laurencin@gmail.com>
2011-06-26 09:52:16 +0000

    * correct max tabs on open cat * added an option to show/hide the cat names

--- hfr-drap4chrome/prefs.js
+++ hfr-drap4chrome/prefs.js
@@ -10,6 +10,7 @@ var OPEN_CAT = "open_cat";
 var MUTED_TOPICS = "muted_topics";
 var BG_COLOR = "bg_color";
 var MAX_OPEN_ALL = "max_open_all";
+var SHOW_CAT = "show_cat"
 
 var PREFS_DEFAULT = new Object();
 PREFS_DEFAULT[USE_DIRECT_LINK] = 'false';
@@ -23,6 +24,7 @@ PREFS_DEFAULT[OPEN_CAT] = 'true';
 PREFS_DEFAULT[MUTED_TOPICS] = '';
 PREFS_DEFAULT[BG_COLOR] = '#2F3740';
 PREFS_DEFAULT[MAX_OPEN_ALL] = 10;
+PREFS_DEFAULT[SHOW_CAT] = 'true';
 
 function getPref(name) {
   var value = localStorage[name];
