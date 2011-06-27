71e2a1bc11881747ed07d61881bf818cbfdd8d2d
brisssou <brice.laurencin@gmail.com>
2011-06-27 21:20:09 +0000

    * added options to retrieve topics/MP or not * clarified some options * bug on get default color gone

--- hfr-drap4chrome/prefs.js
+++ hfr-drap4chrome/prefs.js
@@ -10,7 +10,9 @@ var OPEN_CAT = "open_cat";
 var MUTED_TOPICS = "muted_topics";
 var BG_COLOR = "bg_color";
 var MAX_OPEN_ALL = "max_open_all";
-var SHOW_CAT = "show_cat"
+var SHOW_CAT = "show_cat";
+var GET_TOPICS = "get_topics";
+var GET_MPS = "get_mps";
 
 var PREFS_DEFAULT = new Object();
 PREFS_DEFAULT[USE_DIRECT_LINK] = 'false';
@@ -25,6 +27,8 @@ PREFS_DEFAULT[MUTED_TOPICS] = '';
 PREFS_DEFAULT[BG_COLOR] = '#2F3740';
 PREFS_DEFAULT[MAX_OPEN_ALL] = 10;
 PREFS_DEFAULT[SHOW_CAT] = 'true';
+PREFS_DEFAULT[GET_TOPICS] = 'true';
+PREFS_DEFAULT[GET_MPS] = 'true';
 
 function getPref(name) {
   var value = localStorage[name];
