e5b97449372fda4ccdfe36d3f28bbb3859291211
Brice Laurencin <brisssou@users.noreply.github.com>
2025-08-25 21:55:21 +0200

    updated to latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,37 @@
-//// Add compose gradle plugin
-//plugins {
-//    kotlin("multiplatform")
-//    id("org.jetbrains.compose")
-//}
+plugins {
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlinMultiplatform)
+}
+
+group = "com.composeweb.chrome"
+version = "1.0.0-alpha01"
+
+repositories {
+    mavenCentral()
+    google()
+    gradlePluginPortal()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+//composeCompiler {
+//    includeSourceInformation = true
 //
-//group = "com.composeweb.chrome"
-//version = "1.0.0-alpha01"
-//
-//// Add maven repositories
-//repositories {
-//    mavenCentral()
-//    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+//    featureFlags = setOf(
+//        ComposeFeatureFlag.StrongSkipping.disabled(),
+//        ComposeFeatureFlag.OptimizeNonSkippingGroups
+//    )
 //}
-//
-//kotlin {
-//    js(IR) {
-//        browser()
-//        binaries.executable()
-//    }
-//    sourceSets {
-//        val jsMain by getting {
-//            dependencies {
-//                implementation(compose.web.core)
-//                implementation(compose.runtime)
-//            }
-//        }
-//    }
-//}
+kotlin {
+    js(IR) {
+        browser()
+        binaries.executable()
+    }
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.html.core)
+                implementation(compose.runtime)
+            }
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,9 @@
 pluginManagement {
-    val kotlinMppVersion: String by settings
     repositories {
         gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
     plugins {
-        kotlin("multiplatform") version kotlinMppVersion
-        id("org.jetbrains.compose") version "1.7.3"
     }
 }
 rootProject.name = "compose-chrome-extension-template"
