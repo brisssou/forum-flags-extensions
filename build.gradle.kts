edfe4fc61a88d3112156772b13defc9281074838
Brice Laurencin <brice.laurencin@gmail.com>
2025-10-26 11:35:45 +0100

    set group and version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,11 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
 }
 
-group = "com.composeweb.chrome"
-version = "1.0.0-alpha01"
+allprojects {
+    group = "com.myflags"
+    version = "2.0.0-alpha01"
+}
+
 subprojects {
     repositories {
         mavenCentral()
@@ -13,6 +16,7 @@ subprojects {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
+
 repositories {
     mavenCentral()
     google()
